e930c78ba707e3b6ac7b9a16fc5c898be4bcbce4
raffibromabar-web <raffibromabar@gmail.com>
2026-06-01 15:48:12 +0700

    menampilkan teks tidak jika tidak ada file

--- iofilewithpath.cpp
+++ iofilewithpath.cpp
@@ -51,3 +51,9 @@ int main()
         //menutup file jika telah selesai di loop
         infile.close();
     }
+
+    //kondisi jika file tidak ada
+    else cout << "Unable to open file";
+    
+    return 0;
+}
