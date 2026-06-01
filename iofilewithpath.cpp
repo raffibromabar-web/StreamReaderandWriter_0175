d9db7ed2154eec4d8fc0d266d4adf9212c56d0b0
raffibromabar-web <raffibromabar@gmail.com>
2026-06-01 15:42:26 +0700

    menambahkan untuk menampilkan teks

--- iofilewithpath.cpp
+++ iofilewithpath.cpp
@@ -7,3 +7,6 @@ int main()
 {
     //deklarasi variabel baris dan NamaFile
     string baris, NamaFile;
+    //membuat input untuk menangkap nama file
+    cout << "Masukkan nama file : ";
+    cin >> NamaFile;
