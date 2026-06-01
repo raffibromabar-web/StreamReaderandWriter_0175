d9db117804a29c199e81f276a6f4388d01f9dd12
raffibromabar-web <raffibromabar@gmail.com>
2026-06-01 15:50:07 +0700

    menambahkan string dan oftream

---
+++ streamreaderandwriter.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+//deklarasi variabel baris untuk menyimpan data string
+string baris;
+ //membuka file dalam mode menulis
+ofstream outfile;
+//menunjuk ke sebuah nama file
+outfile.open("contohfile.txt");
+cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+//unlimited loop untuk menulis file
